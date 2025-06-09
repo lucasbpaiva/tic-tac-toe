f13a2b517f48de10e59578914eb22f28cfaf2940
Lucas Paiva <lpaiva234@gmail.com>
2025-06-09 11:20:38 -0300

    add methods for playing game on console

--- script.js
+++ script.js
@@ -21,9 +21,10 @@ function Gameboard() {
 
     const placeToken = (row, column, token) => {
         // prevent placing token on already filled square
-        if (BOARD[row][column] != "") return;
-        
+        if (BOARD[row][column] != "") return "fail";
+
         BOARD[row][column] = token;
+        return "success";
     };
 
     // This method will be used to print our board to the console.
@@ -36,3 +37,55 @@ function Gameboard() {
 
     return {getBoard, placeToken, printBoard};
 }
+
+// The GameController will be responsible for controlling the 
+// flow and state of the game's turns, as well as whether
+// anybody has won the game
+
+function GameController(playerOneName = "Player One", playerTwoName = "Player Two") {
+    const board = Gameboard();
+
+    const players = [
+        {
+            name: playerOneName,
+            token: "X"
+        },
+        {
+            name: playerTwoName,
+            token: "O"
+        }
+    ];
+
+    let activePlayer = players[0];
+
+    const switchPlayerTurn = () => {
+        activePlayer = (activePlayer == players[0] ? players[1] : players[0]);
+    };
+
+    const getActivePlayer = () => activePlayer;
+
+    const printNewRound = () => {
+        board.printBoard();
+        console.log(`${getActivePlayer().name}'s turn.`);
+    };
+
+    const playRound = (row, column) => {
+        //place token of current player
+        console.log(`Placing ${getActivePlayer().name}'s token on the board...`);
+        let play = board.placeToken(row, column, getActivePlayer().token);
+
+        // This is where we would check for a winner and handle that logic,
+        // such as a win message.
+
+        //Switch player turn
+        if (play == "success") switchPlayerTurn();
+        printNewRound();
+    };
+
+    //Initial play game message
+    printNewRound();
+
+    return {getActivePlayer, playRound};
+}
+
+const game = GameController();
