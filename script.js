0b2802d18079d6ef02c804327569a2b10d483630
Lucas Paiva <lpaiva234@gmail.com>
2025-06-11 00:54:43 -0300

    add event listener for restart button

--- script.js
+++ script.js
@@ -15,7 +15,6 @@ function Gameboard() {
         }
     }
 
-
     // This will be the method of getting the entire board that our
     // UI will eventually need to render it.
     const getBoard = () => BOARD;
